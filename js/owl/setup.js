c9c41008976601eb5cc97a92d4249f2959a3df71
gabryelfhsoares <gabryelfhsoares@gmail.com>
2021-08-31 13:46:37 -0300

    add

---
+++ js/owl/setup.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 8,
+  nav: false,
+  responsive: {
+    0: {
+      items: 2.3
+    },
+    600: {
+      items: 4.3
+    },
+    1000: {
+      items: 8.3
+    }
+  }
+})
